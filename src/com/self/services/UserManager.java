5bf74b68b4b73b2de3221906f11feec376f9f305
leol1984 <likang261553@vip.qq.com>
2016-09-08 08:43:06 +0800

    commit
    Change-Id: I57b9caa24c8d1091433dba534279a2e5adb549ea

---
+++ src/com/self/services/UserManager.java
@@ -0,0 +1,9 @@
+package com.self.services;
+
+import com.self.beans.UserForm;
+
+public interface UserManager {
+
+	public void regUser(UserForm user);
+
+}
---
+++ src/com/self/services/impl/UserManagerImpl.java
@@ -0,0 +1,14 @@
+package com.self.services.impl;
+
+import com.self.beans.UserForm;
+import com.self.services.UserManager;
+
+public class UserManagerImpl implements UserManager {
+
+	@Override
+	public void regUser(UserForm user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
